eda504339e2fa7cdaa484d238dcb92e77c12a8cc
uddin_sapienza <uddin@dis.uniroma1.it>
2019-11-08 23:17:37 +0600

    source ino added

---
+++ source/Blink_LED/Blink_LED.ino
@@ -0,0 +1,16 @@
+int LEDpin = 2; // pin declaration
+
+void setup() {
+  pinMode(LEDpin, OUTPUT); // pin setup
+  Serial.begin(115200);   // serial port setup 
+  }
+
+void loop() {
+  digitalWrite(LEDpin,HIGH);   
+  Serial.println("LED is ON");
+  delay(1000);
+  digitalWrite(LEDpin, LOW);
+  Serial.println("LED is OFF");
+  delay(1000);
+  
+}
---
+++ source/analogRead/analogRead.ino
@@ -0,0 +1,17 @@
+// Declare the Analog PIN
+int analogPin = A0;
+int value;
+
+void setup() {
+  pinMode(analogPin, INPUT);
+  Serial.begin(115200);
+  
+}
+
+void loop() {
+  Serial.println("Reading Analog Data....");
+  value = analogRead(analogPin);
+  Serial.println(value);
+  delay(1000);
+
+}
